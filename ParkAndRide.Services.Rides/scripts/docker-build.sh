5c116518f1b5ef0d85e531fd7752901e621684ce
ace1993 <ace.aleks1993@gmail.com>
2019-07-25 09:01:00 +0200

    Docker trying to be configured

--- ParkAndRide.Services.Rides/scripts/docker-build.sh
+++ ParkAndRide.Services.Rides/scripts/docker-build.sh
@@ -1,3 +1,4 @@
 ﻿#!/bin/bash
 #this shell script is used only from the build-all.sh file, it WONT work if you run it solo.
-docker build -t parkandride.services.rides -f Dockerfile ..
+#--no-cache
+docker build --no-cache -t parkandride.services.rides -f Dockerfile ..
